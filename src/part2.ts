6bcd751dd3aa5f3fbf4035d47a9742b6a0fc1f83
Tamar <tamar.shefi11@gmail.com>
2026-04-04 14:46:32 +0300

    added some missing types

--- src/part2.ts
+++ src/part2.ts
@@ -1,11 +1,12 @@
 import * as R from "ramda";
 
 const stringToArray = R.split("");
-//helper function
+
+//helper functions:
 export const isVowel: (letter: string) => boolean = (letter: string): boolean => R.includes(letter, vowels);;
 export const isLetterOrDigit: (ch: string) => boolean = (ch: string): boolean => 
     (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9');;
-export const lettersAndNumsArray = R.pipe(R.toLower, stringToArray, R.filter(isLetterOrDigit));
+export const toLettersAndNumsArray: (s: string) => string[] = R.pipe(R.toLower, stringToArray, R.filter(isLetterOrDigit));;
 
 /* Question 2.1 */
 const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
@@ -14,13 +15,11 @@ export const countVowels: (s: string) => number =
 
 /* Question 2.2 */
 export const isPalindrome = (text: string): boolean => {
-    const textArray = lettersAndNumsArray(text);
+    const textArray = toLettersAndNumsArray(text);
     const reversed = textArray.reduceRight((acc: string, curr: string) => acc + curr, "");
     return textArray.join("") === reversed;
 }
     
-  
-
 /* Question 2.3 */
 export type WordTree = {
     root: string;
@@ -31,9 +30,3 @@ export const treeToSentence = (t: WordTree): string => {
     const s = t.root;
     return R.isEmpty(t.children) ? s : s.concat(" ").concat((R.map(treeToSentence, t.children)).join(" "));
 };;
-
-
-
-
-
-
