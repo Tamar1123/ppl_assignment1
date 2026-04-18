6409b0280e4ad5cc789187010b940045bd1e48b2
Tamar <tamar.shefi11@gmail.com>
2026-04-18 14:27:03 +0300

    added a submit directory only with the requested files

---
+++ submit/src/part2.ts
@@ -0,0 +1,32 @@
+import * as R from "ramda";
+
+const stringToArray = R.split("");
+
+//helper functions:
+export const isVowel: (letter: string) => boolean = (letter: string): boolean => R.includes(letter, vowels);;
+export const isLetterOrDigit: (ch: string) => boolean = (ch: string): boolean => 
+    (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9');;
+export const toLettersAndNumsArray: (s: string) => string[] = R.pipe(R.toLower, stringToArray, R.filter(isLetterOrDigit));;
+
+/* Question 2.1 */
+const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
+export const countVowels: (s: string) => number = 
+    R.pipe(R.toLower, stringToArray, R.filter(isVowel), R.length);
+
+/* Question 2.2 */
+export const isPalindrome = (text: string): boolean => {
+    const textArray = toLettersAndNumsArray(text);
+    const reversed = textArray.reduceRight((acc: string, curr: string) => acc + curr, "");
+    return textArray.join("") === reversed;
+}
+    
+/* Question 2.3 */
+export type WordTree = {
+    root: string;
+    children: WordTree[];
+}
+
+export const treeToSentence = (t: WordTree): string => {
+    const s = t.root;
+    return R.isEmpty(t.children) ? s : s.concat(" ").concat((R.map(treeToSentence, t.children)).join(" "));
+};;
